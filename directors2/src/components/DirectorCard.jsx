44988c72c24019166bd7e9906d02907349fccee2
Naguib10 <65870143+Naguib10@users.noreply.github.com>
2022-12-29 20:10:42 +0200

    Update DirectorCard.jsx

--- directors2/src/components/DirectorCard.jsx
+++ directors2/src/components/DirectorCard.jsx
@@ -5,12 +5,12 @@ function TextExample(props) {
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">Director</Card.Subtitle>
                 <Card.Text>
                     Born in {props.dob}
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                {/* <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link> */}
             </Card.Body>
         </Card>
     );
